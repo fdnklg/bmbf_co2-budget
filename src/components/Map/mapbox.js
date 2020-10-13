263697b2d0e21905a3faa782a79f0dc5b79e6cc5
fdnklg <mail@fabiandinklage.com>
2020-10-13 19:36:12 +0200

    lala

--- src/components/Map/mapbox.js
+++ src/components/Map/mapbox.js
@@ -2,9 +2,9 @@ import mapbox from "mapbox-gl";
 
 console.log(__env__, process);
 
-mapbox.accessToken = __env__.isProd
-  ? process.env.MAPBOX_TOKEN
-  : __env__.MAPBOX_ACCESS_TOKEN;
+// mapbox.accessToken = __env__.isProd
+//   ? process.env.MAPBOX_TOKEN
+//   : __env__.MAPBOX_ACCESS_TOKEN;
 
 const key = {};
 
