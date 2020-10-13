3060db5d9a0f9871cf7a70853b0f9cee23ccc8f7
fdnklg <mail@fabiandinklage.com>
2020-10-13 19:19:17 +0200

    set env token var to vercel

--- src/components/Map/mapbox.js
+++ src/components/Map/mapbox.js
@@ -1,6 +1,8 @@
 import mapbox from "mapbox-gl";
 
-mapbox.accessToken = __env__.MAPBOX_ACCESS_TOKEN;
+mapbox.accessToken = __env__.isProd
+  ? process.env.MAPBOX_TOKEN
+  : __env__.MAPBOX_ACCESS_TOKEN;
 
 const key = {};
 
