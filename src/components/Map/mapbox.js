07b95398977edee43ce05121893756bddd410a74
fdnklg <mail@fabiandinklage.com>
2020-12-02 16:46:00 +0100

    hard code mapbox token

--- src/components/Map/mapbox.js
+++ src/components/Map/mapbox.js
@@ -1,8 +1,10 @@
 import mapbox from "mapbox-gl";
 
-mapbox.accessToken = __env__.isProd
-  ? $MAPBOX_TOKEN
-  : __env__.MAPBOX_ACCESS_TOKEN;
+// mapbox.accessToken = __env__.isProd
+//   ? process.env.MAPBOX_TOKEN
+//   : __env__.MAPBOX_ACCESS_TOKEN;
+
+mapbox.accessToken = 'pk.eyJ1IjoiZmRua2xnIiwiYSI6ImNqeXptazB6czAzM3QzY215bG4xZ242dHEifQ.5ovP53lbKKEVb0OghgtQig'
 
 const key = {};
 
